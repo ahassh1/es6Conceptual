bfb4266563d3dbe0cc5ba2d30fb97653cc32b595
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-04-09 13:37:51 +0600

    pass by value and pass by referance and premitive non-premitive data type done

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 // primitive and non-primitive data type 
 
 // primitive
-// const name= 'polash'
-// const num = 3;
+const name= 'polash'
+const num = 3;
 
-//non-primitive
-// const arr =[ 34,56,67,88,77]
-// const obj ={
-//     name: 'person',
-//     age:34
-// }
+// non-primitive
+const array =[ 34,56,67,88,77]
+const obj ={
+    name: 'person',
+    age:34
+}
 
 // pass by value & pass by reference .
  
 // pass by value 
-const name  = 'polash';
+const nam  = 'polash';
 
 function greet(val){
     console.log(val)
     val= 20
     console.log('hello',val)
 }
-greet(name)
+console.log(nam)
+greet(nam)
+console.log(nam)
 
 // pass by reference .
+const arr =[10,20,30,40];
+function add(arr){
+    console.log(arr)
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        arr[index]= element*element
+        
+    }
+    console.log(arr)
+}
+console.log(arr)
+add(arr)
+console.log(arr)
