e4a85175c2cac19b7dc81ba97522443a4d4009f9
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-04-09 13:55:30 +0600

    double equal and tripple equal and var let const method

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ const obj ={
     name: 'person',
     age:34
 }
-
+////////////////////////////////////////////////////////
 // pass by value & pass by reference .
  
 // pass by value 
 const nam  = 'polash';
 
 function greet(val){
-    console.log(val)
+   
     val= 20
     console.log('hello',val)
 }
@@ -28,7 +28,6 @@ console.log(nam)
 // pass by reference .
 const arr =[10,20,30,40];
 function add(arr){
-    console.log(arr)
     for (let index = 0; index < arr.length; index++) {
         const element = arr[index];
         arr[index]= element*element
@@ -39,3 +38,41 @@ function add(arr){
 console.log(arr)
 add(arr)
 console.log(arr)
+////////////////////////////////////////////////////////////
+
+// == and ===
+console.log(10=='10');
+console.log(10==='10')
+
+
+// var let const 
+
+function main(){
+    var x=10;
+    if(x==10){
+        var y=20;
+        console.log(x+y)
+    }
+    console.log(y)
+}
+main()
+
+function main(){
+    let x=10;
+    if(x==10){
+        var y=20;
+        console.log(x+y)
+    }
+    console.log(y)
+}
+main()
+
+function main(){
+    var x=10;
+    if(x==10){
+        const y=20;
+        console.log(x+y)
+    }
+    console.log(y)
+}
+main()
